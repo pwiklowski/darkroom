f00f4fb30eaa53e8b48c1f1cbcbcce70cdacc2b2
Pawel Wiklowski <pawel.wiklowski@gmail.com>
2016-12-06 20:29:04 +0100

    Add firebase to fronend

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -13,6 +13,17 @@ import { MdlModule } from 'angular2-mdl';
 
 import { GalleriesComponent } from './galleries.component';
 import { GalleryComponent } from './gallery.component';
+import { AngularFireModule, AuthMethods, AuthProviders } from "angularfire2";
+import { BackendService } from './backend.service';
+
+var config = {
+    apiKey: "AIzaSyCpISaYT7x11mYNQJSsTOoYUrUfiIOLIwI",
+    authDomain: "darkroom-6977b.firebaseapp.com",
+    databaseURL: "https://darkroom-6977b.firebaseio.com",
+    storageBucket: "darkroom-6977b.appspot.com",
+    messagingSenderId: "411805980860"
+}
+
 
 @NgModule({
   bootstrap: [ AppComponent ],
@@ -27,10 +38,15 @@ import { GalleryComponent } from './gallery.component';
     HttpModule,
     Ng2BootstrapModule,
     MdlModule,
+    AngularFireModule.initializeApp(config, {
+      provider: AuthProviders.Google,
+      method: AuthMethods.Popup
+    }),
     RouterModule.forRoot(ROUTES, { useHash: true })
   ],
   providers: [ // expose our Services and Providers into Angular's dependency injection
-    ENV_PROVIDERS
+    ENV_PROVIDERS,
+    BackendService
   ]
 })
 export class AppModule {
---
+++ frontend/src/app/backend.service.ts
@@ -0,0 +1,65 @@
+import { Injectable } from '@angular/core';
+import { Http, Response, Headers } from '@angular/http';
+import { AngularFire, AuthProviders } from 'angularfire2';
+
+
+@Injectable()
+export class BackendService{
+    user = {};
+    isLogged = false;
+
+    constructor(private http: Http, public af: AngularFire){
+        this.af.auth.subscribe(user => {
+            if(user) {
+                this.isLogged = true;
+                this.user = user;
+            }
+        });
+    }
+
+    login(){
+        this.af.auth.login({
+            provider: AuthProviders.Google
+        });
+    }
+    logout(){
+        this.af.auth.logout();
+        this.user = {};
+        this.isLogged = false;
+    }
+
+
+    get(url){
+        return new Promise<Response>((resolve, reject)=>{
+            this.af.auth.subscribe(user => {
+                user.auth.getToken().then((token) => {
+                    let h = new Headers({ 'Authorization': token });
+                    return this.http.get(url, {headers: h}).toPromise().then(res => resolve(res)).catch(err => reject(err));
+                });
+            });
+        });
+    }
+
+    post(url, data){
+        return new Promise<Response>((resolve, reject)=>{
+            this.af.auth.subscribe(user => {
+                user.auth.getToken().then((token) => {
+                    let h = new Headers({ 'Authorization': token });
+                    return this.http.post(url, data, {headers: h}).toPromise().then(res => resolve(res)).catch(err => reject(err));
+                });
+            });
+        });
+    }
+
+    delete(url){
+        return new Promise<Response>((resolve, reject)=>{
+            this.af.auth.subscribe(user => {
+                user.auth.getToken().then((token) => {
+                    let h = new Headers({ 'Authorization': token });
+                    return this.http.delete(url, {headers: h}).toPromise().then(res => resolve(res)).catch(err => reject(err));
+                });
+            });
+        });
+    }
+
+}
