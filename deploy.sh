eda477698716c4b7c100efd13f43fec940014483
Pawel Wiklowski <pawel.wiklowski@gmail.com>
2017-01-07 09:41:54 +0100

    Update build scripts

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,8 @@
 export DOCKER_HOST=tcp://192.168.0.11:5000
 
-export FIREBASE_CONFIG=cred.json
-export PHOTOS_LOCATION=/hdd/temp/darkroom_prod
-export DATABASE_LOCATION=/home/pawwik/dev/darkroom_go/mydatabase
+export FIREBASE_CONFIG=/cred.json
+export PHOTOS_LOCATION=/home/pwiklowski/darkroom/data
+export DATABASE_LOCATION=/home/pwiklowski/darkroom/db
 
 sh build.sh
 docker-compose up -d
--- deploy_local.sh
+++ deploy_local.sh
@@ -1,6 +1,9 @@
+#!/bin/sh
+
+
 export FIREBASE_CONFIG=/cred.json
 export PHOTOS_LOCATION=/hdd/temp/darkroom_prod
 export DATABASE_LOCATION=/home/pawwik/dev/darkroom_go/mydatabase
 
-#build.sh
+./build.sh
 docker-compose up -d
