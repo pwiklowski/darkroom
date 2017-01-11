89836c5c82d9e7388ad5786e4126bff99ccdcbe8
Pawel Wiklowski <pawel.wiklowski@gmail.com>
2017-01-11 19:26:26 +0100

    Remove deploy scripts

--- deploy.sh
+++
@@ -1,8 +0,0 @@
-export DOCKER_HOST=tcp://192.168.0.11:5000
-
-export FIREBASE_CONFIG=/cred.json
-export PHOTOS_LOCATION=/home/pwiklowski/darkroom/data
-export DATABASE_LOCATION=/home/pwiklowski/darkroom/db
-
-sh build.sh
-docker-compose up -d
--- deploy_local.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-
-export FIREBASE_CONFIG=/cred.json
-export PHOTOS_LOCATION=/hdd/temp/darkroom_prod
-export DATABASE_LOCATION=/home/pawwik/dev/darkroom_go/mydatabase
-
-./build.sh
-docker-compose up -d
