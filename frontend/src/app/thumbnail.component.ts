312f99370e47260d973dad931b8b3516f9f84619
Pawel Wiklowski <pawel.wiklowski@gmail.com>
2017-02-06 17:00:29 +0100

    Add hover effect on photo

--- frontend/src/app/thumbnail.component.ts
+++ frontend/src/app/thumbnail.component.ts
@@ -5,19 +5,39 @@ import { BackendService } from './backend.service';
 
 @Component({
     selector: 'thumbnail',
-    template:` <img #photo class="dr-photo" src="assets/img/stub2.gif">`,
+    template:`<div class="dr-thumbnail-container">
+                <img #photo class="dr-thumbnail" src="assets/img/stub2.gif" (mouseover)="showVotes(true)" (mouseout)="showVotes(false)">
+              </div>`,
     styles: [`
-    .dr-photo {
-      opacity: 0.1;
-      transition: opacity 1000ms ease-in-out;
-      width: 100%;
+    .dr-thumbnail-container{
+      overflow: hidden;
+      position: relative;
     }
+
+    .dr-thumbnail{
+      opacity: 0.1;
+      width: 100%;
+      cursor: zoom-in;
+      transition: opacity 1000ms ease-in-out;
+      transition: all 200ms ease-in-out;
+    }
+    .dr-thumbnail:hover{
+      transform: scale(1.1);
+
+      -webkit-filter: blur(2px);
+      -moz-filter: blur(2px);
+      -o-filter: blur(2px);
+      -ms-filter: blur(2px);
+      filter: blur(2px);
+    }
+
     `]
 })
 export class ThumbnailComponent{
   @Input('pid') photoId: string;
   @Input('index') index: number;
   @ViewChild('photo') photo;
+  @ViewChild('photoContainer') photoContainer;
 
 
   constructor(private backend: BackendService){
